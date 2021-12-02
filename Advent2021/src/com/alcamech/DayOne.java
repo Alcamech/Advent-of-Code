5b4e9e0cf0907a30a1efc171bf379c13dde68080
Lawton Mizell <lmizell@redhat.com>
2021-12-02 08:35:20 -0500

    advent of code 2021: day one solution

---
+++ Advent2021/src/com/alcamech/DayOne.java
@@ -0,0 +1,55 @@
+package com.alcamech;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayOne {
+    String file = getClass().getClassLoader().getResource("advent-day-1-input.txt").getFile();
+
+    public static List<Integer> createNumListFromFile(String file) {
+        List<Integer> numList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                numList.add(Integer.parseInt(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numList;
+    }
+
+    public int SolutionPartOne() {
+        List<Integer> numList = createNumListFromFile(file);
+        int answer = 0;
+        int prevDepth = numList.get(0);
+
+        for (Integer integer : numList) {
+            if (integer > prevDepth) {
+                answer++;
+            }
+            prevDepth = integer;
+        }
+        return answer;
+    }
+
+    public int SolutionPartTwo() {
+        int answer = 0;
+        List<Integer> numList = createNumListFromFile(file);
+        int prevCalc = numList.get(0) + numList.get(1) + numList.get(2);
+        for(int i=0; i<numList.size()-2; i++) {
+            int sum = numList.get(i) + numList.get(i+1) + numList.get(i+2);
+
+            if(sum > prevCalc) {
+                answer++;
+            }
+
+            prevCalc = sum;
+        }
+
+        return answer;
+    }
+}
---
+++ Advent2021/src/com/alcamech/Main.java
@@ -0,0 +1,10 @@
+package com.alcamech;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DayOne dayOne = new DayOne();
+        System.out.println(dayOne.SolutionPartOne());
+        System.out.println(dayOne.SolutionPartTwo());
+    }
+}
